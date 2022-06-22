30f3fad4a909e7b3691eaf2a1701913f41cbd3d6
Michel Meyer <meyer_michel@outlook.com>
2022-06-22 22:35:58 +0200

    add bazel build with cmake

---
+++ app/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+ 
+int main(int argc, char *argv[])
+{
+    std::cout << "HELLO World app" << argc << std::endl;
+    return EXIT_SUCCESS; 
+}
